590fb42e6fc83d7ce41e2e45a6fa42bdfdf4581d
Durga Mukesh <mukeshnaidupothula@gmail.com>
2023-05-22 11:00:59 +0400

    UI Updates as per the Dashboard analytics

--- controller/self_web.py
+++ controller/self_web.py
@@ -1,5 +1,4 @@
-from controller.save_frame_info_to_excel import save_frame_info_to_excel
-from position_detector import points
+from flask import Flask, request
 import cv2
 from ultralytics import YOLO
 import pandas as pd
@@ -7,70 +6,68 @@ import numpy as np
 from tracker import Tracker
 import random
 import time
-import datetime
+
+app = Flask(__name__)
 
 
-# Yolo Model Initialization
-model = YOLO('../yolov8s.pt')
+@app.route('/process_video', methods=['POST'])
+def process_video():
+    # Position Detector Function
+    def points(event, x, y, flags, param):
+        pass
 
-# Detect and use Live Camera for Video Capture
-video_path = '../assets/Videos/selfcheckout.mp4'
-video = cv2.VideoCapture(video_path)
+    # Yolo Model Initialization
+    model = YOLO('../yolov8s.pt')
 
-# Frame Width and Height
-frame_width = int(video.get(3))
-frame_height = int(video.get(4))
+    # Read the video file from the request
+    video_file = request.files['video']
+    video = cv2.VideoCapture(video_file)
 
-# Custom Class List from COCO File
-my_file = open("../assets/files/coco.txt", "r")
-data = my_file.read()
-class_list = data.split("\n")
+    # Frame Width and Height
+    frame_width = int(video.get(3))
+    frame_height = int(video.get(4))
 
-# To Get Window points Custom Class
-cv2.setMouseCallback('VisionSense', points)
+    # Custom Class List from COCO File
+    my_file = open("../assets/files/coco.txt", "r")
+    data = my_file.read()
+    class_list = data.split("\n")
 
-tracker = Tracker()
-colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for j in range(10)]
+    # To Get Window points Custom Class
+    cv2.setMouseCallback('VisionSense', points)
 
+    tracker = Tracker()
+    colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for j in range(10)]
 
-track_timings = {}
-drop_out = 0
+    track_timings = {}
+    drop_out = 0
 
-checkout_area1 = [(627, 375), (574, 583), (978, 643), (1001, 407)]
-checkout_area2 = [(569, 602), (530, 752), (957, 813), (971, 656)]
+    pos_area = [(627, 375), (574, 583), (978, 643), (1001, 407)]
+    staff_area = [(569, 602), (530, 752), (957, 813), (971, 656)]
 
-que_position1 = 0
-que_position2 = 0
-que_position3 = 0
+    que_position1 = 0
+    que_position2 = 0
+    que_position3 = 0
 
-camera_id = 1
-camera_name = 'self-checkout'
+    detection_threshold = 0.5
 
-# Read and process each frame
-frame_count = 0
+    while video.isOpened():
+        # frame refers Current Image Frame from Video
+        ret, image = video.read()
 
-detection_threshold = 0.5
-
-while video.isOpened():
-    # frame refers Current Image Frame from Video
-    ret, image = video.read()
-
-    # If the image was not read successfully, end the loop
-    if not ret:
-        break
-
-    if frame_count % 3 == 0:
+        # If the image was not read successfully, end the loop
+        if not ret:
+            break
 
         # Resize Image according to the requirements
         image = cv2.resize(image, (1920, 1080))
 
         # Yolov8 Package Results of each Image Frame
-        # yolov8Results = model.predict(image)
+        # yolov8_results = model.predict(image)
 
         # Yolov8 Package Results of each Image Frame Custom Classes
-        yolov8Results = model.predict(source=image, show=False, stream=True, classes=[0])
+        yolov8_results = model.predict(source=image, show=False, stream=True, classes=[0])
 
-        for i, (result) in enumerate(yolov8Results):
+        for i, (result) in enumerate(yolov8_results):
             a = result.boxes.data
             px = pd.DataFrame(a).astype("float")
 
@@ -97,8 +94,7 @@ while video.isOpened():
                 cx = int(x1 + x2) // 2
                 cy = int(y1 + y2) // 2
 
-                results = cv2.pointPolygonTest(np.array(checkout_area1, np.int32), (cx, cy), False)
-                frame_count += 1
+                results = cv2.pointPolygonTest(np.array(pos_area, np.int32), (cx, cy), False)
 
                 if results >= 0:
                     cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (colors[track_id % len(colors)]), 3)
@@ -113,17 +109,13 @@ while video.isOpened():
                         cv2.putText(image, str(f"Track ID {track_id} spent {time_spent} seconds on the screen"),
                                     (1381, 892), cv2.FONT_HERSHEY_COMPLEX, 1,
                                     (255, 0, 0), 2)
-
-                        # Save frame information to Excel
-                        save_frame_info_to_excel(camera_id, camera_name, frame_count, datetime.datetime.now(), time_spent, track_id, 'A')
-
                     else:
                         # Object is new, add it to the track timings dictionary with the current centroid and entry time
                         track_timings[track_id] = (time.time())
 
                     que_position1 += 1
 
-                results1 = cv2.pointPolygonTest(np.array(checkout_area2, np.int32), (cx, cy), False)
+                results1 = cv2.pointPolygonTest(np.array(staff_area, np.int32), (cx, cy), False)
 
                 if results1 >= 0:
                     cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (colors[track_id % len(colors)]), 3)
@@ -139,9 +131,6 @@ while video.isOpened():
                                     (1381, 986), cv2.FONT_HERSHEY_COMPLEX, 1,
                                     (255, 0, 0), 2)
 
-                        # Save frame information to Excel
-                        save_frame_info_to_excel(camera_id, camera_name, frame_count, datetime.datetime.now(), time_spent, track_id, 'B')
-
                         # Check if the staff area was reached before the position area
                         if track_id in track_timings and track_timings[track_id] < track_timings.get(track_id, 0):
                             drop_out += 1
@@ -152,10 +141,12 @@ while video.isOpened():
 
                     que_position2 += 1
 
-        cv2.putText(image, str(f'Staff Count: {int(que_position1)}'), (20, 80), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
-        cv2.putText(image, str(f'POS Count: {int(que_position2)}'), (20, 40), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
-        cv2.polylines(image, [np.array(checkout_area1, np.int32)], True, (0, 0, 255), 3)
-        cv2.polylines(image, [np.array(checkout_area2, np.int32)], True, (255, 0, 0), 3)
+        cv2.putText(image, str(f'Staff Count: {int(que_position1)}'), (20, 80), cv2.FONT_HERSHEY_COMPLEX, 1,
+                    (255, 0, 0), 2)
+        cv2.putText(image, str(f'POS Count: {int(que_position2)}'), (20, 40), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255),
+                    2)
+        cv2.polylines(image, [np.array(pos_area, np.int32)], True, (0, 0, 255), 3)
+        cv2.polylines(image, [np.array(staff_area, np.int32)], True, (255, 0, 0), 3)
 
         # Show Image
         cv2.imshow('VisionSense', image)
@@ -164,7 +155,11 @@ while video.isOpened():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-frame_count += 1
+    video.release()
+    cv2.destroyAllWindows()
 
-video.release()
-cv2.destroyAllWindows()
+    return 'Processing completed'
+
+
+if __name__ == '__main__':
+    app.run()
